77bf74bef0cb357ae9d8b2cb7f5c1e00dea44674
Yatin Gupta <yatingupta7777@gmail.com>
2022-11-24 15:45:30 -0600

    fix max bwidth code

--- Utils.cpp
+++ Utils.cpp
@@ -30,7 +30,7 @@ void printBandwidth(int dad[], int bwidth[], int source, int destination)
     int max_bwidth = INT_MAX;
     while (dad[x] != -1)
     {
-        max_bwidth = min(INT_MAX, bwidth[x]);
+        max_bwidth = min(max_bwidth, bwidth[x]);
         x = dad[x];
     }
     cout << "Max Bandwidth : " << max_bwidth << endl;
